47061a516ccb5155e458c0296df26aa381c0b2c6
djalil sahraoui <djalilsah@gmail.com>
2020-11-24 18:34:14 +0100

    now the project can be exposed by the home page ;)

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import project
 
-# Create your views here.
+
+def start(request):
+    projects = project.objects.all()
+    return render(request,'portfolio/home.html',{'projects':projects})
+    
